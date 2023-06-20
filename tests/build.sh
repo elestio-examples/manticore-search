dbe29eae4bd277a627a9254bc3345922703011da
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-06-20 11:51:19 +0300

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
+sed -i 's/^ADD \*deb \/packages\//\# ADD \*deb \/packages\//' Dockerfile
+
+mkdir -p -m 750 ./data
+mkdir -p -m 750 ./manticore.conf
+
+chown -R 1000:1000 ./data
+chown -R 1000:1000 ./manticore.conf
+
 docker build . --tag elestio4test/manticoresearch:latest
